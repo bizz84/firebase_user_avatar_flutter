3bcc91d43b1b7cf3f3d4725d77de09b5c4d30bb1
bizz84 <bizz84@gmail.com>
2019-10-09 10:49:40 +0100

    Implement authentication flow

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_user_avatar_flutter/services/firebase_auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SignInPage extends StatelessWidget {
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
-      // TODO: Implement
+      final auth = Provider.of<FirebaseAuthService>(context);
+      await auth.signInAnonymously();
     } catch (e) {
       print(e);
     }
