f0f6519262ba6826edaba0c1294eff441e9e2a2d
bizz84 <bizz84@gmail.com>
2019-10-09 10:24:39 +0100

    Remove completed code from HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,20 +2,12 @@ import 'dart:async';
 
 import 'package:firebase_user_avatar_flutter/app/home/about_page.dart';
 import 'package:firebase_user_avatar_flutter/common_widgets/avatar.dart';
-import 'package:firebase_user_avatar_flutter/models/avatar_reference.dart';
-import 'package:firebase_user_avatar_flutter/services/firebase_auth_service.dart';
-import 'package:firebase_user_avatar_flutter/services/firebase_storage_service.dart';
-import 'package:firebase_user_avatar_flutter/services/firestore_database.dart';
-import 'package:firebase_user_avatar_flutter/services/image_picker_service.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   Future<void> _signOut(BuildContext context) async {
     try {
-      final auth = Provider.of<FirebaseAuthService>(context);
-      await auth.signOut();
+      // TODO: Implement
     } catch (e) {
       print(e);
     }
@@ -32,17 +24,7 @@ class HomePage extends StatelessWidget {
 
   Future<void> _chooseAvatar(BuildContext context) async {
     try {
-      // 1. Get image from picker
-      final imagePicker = Provider.of<ImagePickerService>(context);
-      final file = await imagePicker.pickImage(source: ImageSource.gallery);
-      // 2. Upload to storage
-      final storage = Provider.of<FirebaseStorageService>(context);
-      final downloadUrl = await storage.uploadAvatar(file: file);
-      // 3. Save url to Firestore
-      final database = Provider.of<FirestoreDatabase>(context);
-      await database.setAvatarReference(AvatarReference(downloadUrl));
-      // 4. (optional) delete local file as no longer needed
-      await file.delete();
+      // TODO: Implement
     } catch (e) {
       print(e);
     }
@@ -83,20 +65,13 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _buildUserInfo({BuildContext context}) {
-    // TODO: Remove in tutorial
-    final database = Provider.of<FirestoreDatabase>(context);
-    return StreamBuilder<AvatarReference>(
-      stream: database.avatarReferenceStream(),
-      builder: (context, snapshot) {
-        final avatarReference = snapshot.data;
-        return Avatar(
-          photoUrl: avatarReference?.downloadUrl,
-          radius: 50,
-          borderColor: Colors.black54,
-          borderWidth: 2.0,
-          onPressed: () => _chooseAvatar(context),
-        );
-      },
+    // TODO: Download and show avatar from Firebase storage
+    return Avatar(
+      photoUrl: null,
+      radius: 50,
+      borderColor: Colors.black54,
+      borderWidth: 2.0,
+      onPressed: () => _chooseAvatar(context),
     );
   }
 }
